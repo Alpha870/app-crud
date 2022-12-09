5130d9ae5d4b5192ebf15e276452770d24191d18
Omar_Perez <omar1987perez2@gmail.com>
2022-12-09 11:54:24 +0100

    añadiendo funcionalidad al js y cambiando css

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+// variables
 
+const formularioUI = document.getElementById("selector");
+const listadoUI = document.getElementById("listaRutina");
+const submit = document.getElementById("guardar");
+let arrayActividades = [];
+
+// Funciones
+
+
+// Eventos
+
+formularioUI.addEventListener("change", (e) => {
+  e.preventDefault();
+
+  const memoria = [];
+  let actividadUI = formularioUI.options[formularioUI.selectedIndex].text;
+  memoria.push(actividadUI);
+  
+  
+  submit.addEventListener("click", (e) => {
+      e.preventDefault();
+      console.log(memoria)
+    
+      
+
+  });
+});
